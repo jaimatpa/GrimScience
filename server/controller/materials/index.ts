f0ebdbc59fc7836f6b1bb5bbd851a29dcac6df2e
Stephan Shere <stephan@imagi-tech.com>
2024-08-29 04:37:26 -0700

    Resolving conflicts

--- server/controller/materials/index.ts
+++ server/controller/materials/index.ts
@@ -1,10 +1,4 @@
-<<<<<<< HEAD
 export * from "./InventoryTransactions";
 export * from "./Parts";
 export * from "./Vendors";
 export * from "./purchase";
-=======
-export * from './InventoryTransactions'
-export * from './Parts'
-export * from './Vendors'
->>>>>>> dev
