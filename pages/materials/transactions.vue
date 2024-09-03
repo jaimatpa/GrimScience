5afcef999db635071ea4abaa5066e310089d6193
Sifat-Jaber <sifatjaber@gmail.com>
2024-09-03 18:01:45 +0600

    Initiated Transactions page and implemented Inventory Transactions list item double click actions to render conditional modals (apart from Vendor Invoice modal) in InventoryTransactions component.

--- pages/materials/transactions.vue
+++ pages/materials/transactions.vue
@@ -5,5 +5,9 @@ useSeoMeta({
 </script>
 
 <template>
-      <div class="w-full flex items-center text-center justify-center">This page is under development and not available yet.</div>
+      <!-- <div class="w-full flex items-center text-center justify-center">This page is under development and not available yet.</div> -->
+
+       <div class="w-full flex items-center text-center justify-center">
+          <MaterialsTransactionsInventoryTransactions />
+       </div>
 </template>
