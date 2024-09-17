f759f7793416af2346712b5e2cbed0c7f942e837
Sifat-Jaber <sifatjaber@gmail.com>
2024-09-17 18:49:58 +0600

    implemented backend controller and APIs

--- server/api/engineering/capa/[id].ts
+++ server/api/engineering/capa/[id].ts
@@ -1,24 +1,23 @@
-import { getCapaDetail } from "~/server/controller/engineering/Capa";
+import { getCapaDetail } from '~/server/controller/engineering/Capa'
 
 export default eventHandler(async (event) => {
   try {
-    const method = event._method;
-    const id = event.context.params.id;
+    const method = event._method
+    const id = event.context.params.id
 
-    switch(method){
+    switch (method) {
       case 'GET':
-        const capaDetail = await getCapaDetail(id);
-        if (capaDetail) return { body: capaDetail, message: "" };
+        const capaDetail = await getCapaDetail(id)
+        if (capaDetail) return { body: capaDetail, message: '' }
         else {
-            setResponseStatus(event, 404);
-            return { error: "The Investigation does not exist" };
+          setResponseStatus(event, 404)
+          return { error: 'The Investigation does not exist' }
         }
       default:
-        setResponseStatus(event, 405);
-        return { error: 'Method Not Allowed' };
+        setResponseStatus(event, 405)
+        return { error: 'Method Not Allowed' }
     }
-    
   } catch (error) {
-    throw new Error(`Error fetching data from table: ${error.message}`);
+    throw new Error(`Error fetching data from table: ${error.message}`)
   }
-});
+})
--- server/controller/engineering/Capa.ts
+++ server/controller/engineering/Capa.ts
@@ -1,5 +1,56 @@
-import { Op, Sequelize } from 'sequelize'
-import { tblPreventiveActions } from '~/server/models'
+import { Sequelize, Op, QueryTypes } from 'sequelize'
+import { tblPermissions, tblPreventiveActions, tblWorkCenters } from '~/server/models'
+import sequelize from '~/server/utils/databse'
+
+export const applyPermissions = async (employeeId) => {
+  console.log('Employee ID:', employeeId)
+
+  // Define the system and subsystem for permission checks
+  const system = 'Engineering'
+  const subsystem = 'Approvals'
+
+  // Fetch permissions for the given system and subsystem
+  const permissions = await tblPermissions.findOne({
+    where: {
+      system: system,
+      subsystem: subsystem
+    }
+  })
+
+  // console.log('Permissions:', permissions)
+
+  // If no permissions found, return restricted access
+  if (!permissions) {
+    return { enabled: false, message: 'You are not authorized to access this part of the system.' }
+  }
+
+  let accessLevel = 'Restricted' // Default access level
+  let readOnly = false
+  let message = ''
+
+  // Check for full access based on 'MenuItem' or 'FullAdminIDs'
+  if (permissions.MenuItem === 'Full' || permissions.FullAdminIDs.includes(employeeId)) {
+    accessLevel = 'Full'
+
+    console.log('Full Access:', accessLevel)
+  }
+  // Check for read-only access based on 'MenuItem' or 'ReadOnlyIDs'
+  else if (permissions.MenuItem === 'Read Only' || permissions.ReadOnlyIDs.includes(employeeId)) {
+    accessLevel = 'Read Only'
+    readOnly = true
+    message = 'READ-ONLY ACCESS'
+
+    console.log('Read-Only Access:', accessLevel)
+  }
+
+  // If access level is restricted, deny access
+  if (accessLevel === 'Restricted') {
+    return { enabled: false, message: 'You are not authorized to access this part of the system.' }
+  }
+
+  // Return the determined permissions
+  return { enabled: true, readOnly, message }
+}
 
 const applyFilters = (params) => {
   const filterParams = ['PANO', 'PRODLINE', 'ACTIONTYPE', 'DESCRIPTION']
@@ -47,9 +98,119 @@ export const getCapas = async (params) => {
 }
 
 export const getCapaDetail = async (id) => {
-    console.log('id', id)
-    const capaDetail = await tblPreventiveActions.findByPk(id)
-    
-    console.log('capaDetail', capaDetail)
-  return capaDetail;
+  console.log('id', id)
+  const capaDetail = await tblPreventiveActions.findByPk(id)
+
+  console.log('capaDetail', capaDetail)
+  return capaDetail
+}
+
+export const getCapaComplaints = async (params) => {
+  const { capaId, COMPLAINTDATE, SERIALNO, CUSTOMERNUMBER } = params
+
+  try {
+    // Build the SQL query
+    const sqlQuery = `
+        WITH RankedComplaints AS (
+          SELECT 
+            tblComplaints.complaintdate AS COMPLAINTDATE, 
+            tblComplaints.serialno AS SERIALNO, 
+            tblComplaints.complaintnumber AS COMPLAINTNUMBER, 
+            tblComplaints.uniqueid AS ComplaintID, 
+            tblCustomers.number AS CUSTOMERNUMBER,
+            ROW_NUMBER() OVER (PARTITION BY tblComplaints.uniqueid ORDER BY tblComplaints.complaintdate DESC) AS rn
+          FROM tblComplaints
+          INNER JOIN tblCustomers 
+            ON tblCustomers.uniqueid = tblComplaints.CustomerID
+          INNER JOIN tblPAComplaint 
+            ON tblPAComplaint.ComplaintID = tblComplaints.UniqueID
+          WHERE tblPAComplaint.PreventiveActionID = :capaId
+        )
+        SELECT 
+          COMPLAINTDATE, 
+          SERIALNO, 
+          COMPLAINTNUMBER, 
+          ComplaintID, 
+          CUSTOMERNUMBER
+        FROM RankedComplaints
+        WHERE rn = 1
+          AND (SERIALNO LIKE :SERIALNO)
+          AND (CUSTOMERNUMBER LIKE :CUSTOMERNUMBER)
+        ORDER BY COMPLAINTDATE DESC;
+      `
+
+    // Execute the raw SQL query
+    const complaints = await sequelize.query(sqlQuery, {
+      replacements: {
+        capaId,
+        COMPLAINTDATE: COMPLAINTDATE ? new Date(COMPLAINTDATE).toISOString().slice(0, 10) : '%',
+        SERIALNO: SERIALNO ? `%${SERIALNO}%` : '%',
+        CUSTOMERNUMBER: CUSTOMERNUMBER ? `%${CUSTOMERNUMBER}%` : '%'
+      },
+      type: QueryTypes.SELECT
+    })
+
+    // Format COMPLAINTDATE for each complaint
+    complaints.forEach((complaint) => {
+      complaint.COMPLAINTDATE = complaint.COMPLAINTDATE ? new Date(complaint.COMPLAINTDATE).toLocaleDateString() : null
+    })
+
+    return complaints
+  } catch (error) {
+    console.error('Error fetching complaints:', error)
+    throw error
+  }
+}
+
+export const getInvestigations = async (params) => {
+  const { PreventiveActionID } = params
+
+  try {
+    const sqlQuery = `
+      SELECT 
+        tblInvestigations.uniqueID,
+        tblInvestigations.PANO, 
+        tblInvestigations.PRODLINE, 
+        tblInvestigations.DESCRIPTION AS DESCRIPTION, 
+        tblInvestigations.DIAGDATE AS DIAGDATE 
+      FROM tblPAInvestigation 
+      INNER JOIN tblInvestigations 
+        ON tblPAInvestigation.investigationID = tblInvestigations.uniqueID 
+      INNER JOIN tblPreventiveActions 
+        ON tblPAInvestigation.PreventiveActionID = tblPreventiveActions.uniqueID 
+      WHERE tblPAInvestigation.PreventiveActionID = :PreventiveActionID
+    `
+
+    // Execute the raw SQL query
+    const investigations = await sequelize.query(sqlQuery, {
+      replacements: { PreventiveActionID: parseInt(PreventiveActionID, 10) },
+      type: QueryTypes.SELECT
+    })
+
+    // Format the DiagDate for each investigation
+
+    if (investigations) {
+      investigations.forEach((investigation) => {
+        investigation.DIAGDATE = investigation.DIAGDATE ? new Date(investigation.DIAGDATE).toLocaleDateString() : null
+      })
+    }
+
+    // Return the list of investigations
+    return investigations
+  } catch (error) {
+    console.error('Error fetching investigations:', error)
+    throw error
+  }
+}
+
+export const getWorkCenters = async () => {
+  const workCenters = await tblWorkCenters.findAll({
+    attributes: ['NAME', 'NUMBER'],
+    order: [['NUMBER', 'ASC']]
+  })
+
+  // return each entity in the format #NUMBER NAME
+  return workCenters.map((workCenter) => {
+    return `#${workCenter.NUMBER} ${workCenter.NAME}`
+  })
 }
