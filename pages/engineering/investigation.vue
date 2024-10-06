64ea6a49b357a9a60b07bcffaaab6820618fd22c
Jairo <jairo.pauluz.matos@gmail.com>
2024-10-06 10:27:20 +0600

    investigation page

--- pages/engineering/investigation.vue
+++ pages/engineering/investigation.vue
@@ -5,11 +5,5 @@ useSeoMeta({
 </script>
 
 <template>
-  <!-- <div class="w-full flex items-center text-center justify-center">This page is under development and not available yet.</div> -->
-
-       <EngineeringInvestigationDetail :selected-investigation="null" />
-  <EngineeringInvestigationDetail :selected-investigation="null" />
-  <!-- <div class="w-full flex items-center text-center justify-center">This page is under development and not available yet.</div> -->
-
   <EngineeringInvestigationDetail :selected-investigation="null" />
 </template>
